a0faa3b5840fae4d2249d1dc8eee4b69ff4522ad
Senyanya <mitin190@gmail.com>
2020-03-17 12:47:40 +0300

    First task

--- index.php
+++ index.php
@@ -27,6 +27,23 @@ $user_name = ''; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
+			<?php if($is_auth==1): ?>
+			  <div class="user-menu__image">
+				<img src="img/user.jpg" width="40" height="40" alt="Пользователь">
+				</div>
+				<div class="user-menu__logged">
+				<p></p>
+				</div>
+			<?php else: ?>
+				  <ul class="user-menu__list">
+					<li class="user-menu__item">
+					<a href="#">Регистрация</a>
+					</li>
+					<li class="user-menu__item">
+					<a href="#">Вход</a>
+					</li>
+					</ul>
+			<?php endif; ?>
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 
@@ -73,7 +90,6 @@ $user_name = ''; // укажите здесь ваше имя
     </section>
 </main>
 </div>
-
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
